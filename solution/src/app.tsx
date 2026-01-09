ccb996f0c783aafe0e3f633c4f93f6dbfd468e01
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2026-01-09 16:46:14 +0000

    add api key

--- solution/src/app.tsx
+++ solution/src/app.tsx
@@ -51,7 +51,7 @@ const locations: Poi[] = [
 ];
 
 const App = () => (
-  <APIProvider apiKey={'Your API key here'} onLoad={() => console.log('Maps API has loaded.')}>
+  <APIProvider apiKey={'AIzaSyCZiZTseixjlpxMb3BJgcKzDwywqe3cXxQ'} onLoad={() => console.log('Maps API has loaded.')}>
     <Map
       defaultZoom={13}
       defaultCenter={{ lat: -33.860664, lng: 151.208138 }}
